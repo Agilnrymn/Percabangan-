eb5c70c19e1ed3bc3d68dec77987966f9e4b6551
Agilnrymn <116326929+Agilnrymn@users.noreply.github.com>
2022-10-27 11:16:37 +0700

    Add files via upload

---
+++ Percabangan.php
@@ -0,0 +1,18 @@
+<?php
+$a = 80;
+$b = 81;
+
+if($a % 79){
+    echo 'a adalah bilangan genap';
+}
+else{
+    echo 'a adalah bilangan ganjil';
+}
+echo "<pre>";
+if($b % 80){
+    echo 'b adalah bilangan ganjil';
+}
+else{
+    echo 'b adalah bilangan genap';
+}
+?>
